d2a00e8b773c2703bec52eb91ccadee1541d555d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-12 15:34:00 +0100

    create model entitity Product.java

---
+++ src/main/java/com/openclassrooms/datalayer/model/Product.java
@@ -0,0 +1,30 @@
+package com.openclassrooms.datalayer.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produit")
+public class Product {
+ 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "produit_id")
+	private int productId;
+ 
+	@Column(name = "nom")
+	private String name;
+ 
+	@Column(name = "description")
+	private String description;
+ 
+	@Column(name = "cout")
+	private int cost;
+ 
+ 
+	// Add getters & setters
+}
