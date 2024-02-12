28d6f7896023d1352bc828eea37be660353f7fb0
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-12 18:03:02 +0100

    add method getProducts() in ProductService.java and test in commandlinrunner in main class

--- src/main/java/com/openclassrooms/datalayer/DatalayerApplication.java
+++ src/main/java/com/openclassrooms/datalayer/DatalayerApplication.java
@@ -1,13 +1,25 @@
 package com.openclassrooms.datalayer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DatalayerApplication {
+import com.openclassrooms.datalayer.model.Product;
+import com.openclassrooms.datalayer.service.ProductService;
 
+@SpringBootApplication
+public class DatalayerApplication  implements CommandLineRunner{
+	@Autowired
+	private ProductService productService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DatalayerApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(String... args) throws Exception {		
+		Iterable<Product> products = productService.getProducts();
+		products.forEach(product -> System.out.println(product.getName()));
+	}
 }
--- src/main/java/com/openclassrooms/datalayer/service/ProductService.java
+++ src/main/java/com/openclassrooms/datalayer/service/ProductService.java
@@ -1,8 +1,17 @@
 package com.openclassrooms.datalayer.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.openclassrooms.datalayer.model.Product;
+import com.openclassrooms.datalayer.repository.IProductRepository;
 
 @Service
 public class ProductService {
-
+	@Autowired
+	private IProductRepository productRepository;
+	
+	public Iterable<Product> getProducts() {
+		return productRepository.findAll();
+	}
 }
