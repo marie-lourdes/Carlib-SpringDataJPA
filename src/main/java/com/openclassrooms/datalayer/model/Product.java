0a1c02d22018bf4ef99cff7e9629d1988fff4108
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-12 22:49:29 +0100

    Product.java: add attribut list comments with relation @OneToMany

--- src/main/java/com/openclassrooms/datalayer/model/Product.java
+++ src/main/java/com/openclassrooms/datalayer/model/Product.java
@@ -1,10 +1,15 @@
 package com.openclassrooms.datalayer.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -26,6 +31,13 @@ public class Product {
  
 	@Column(name = "cout")
 	private int cost;
+	
+	@OneToMany(
+			cascade=CascadeType.ALL,
+			orphanRemoval=true,
+			fetch= FetchType.EAGER
+			)
+	private List<Comment> comments;
  
  
 	// Add getters & setters
